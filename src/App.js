c0e35329ebb1904e3815d588deb4a392de665d14
Mangesh <63477318+mangeshkchauhan@users.noreply.github.com>
2022-10-20 16:44:19 +0530

    first build

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react";
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from "./Spinner";
+import './style/App.css'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
+
   render() {
     return (
       <>
         {this.state.errorMessage ? (
           <div>Error: {this.state.errorMessage}</div>
         ) : this.state.lat ? (
-          <div>Latitude: {this.state.lat}</div>
+          <SeasonDisplay lat={this.state.lat}/>
         ) : (
-          <div>Loading...</div>
+          <Spinner message={'Please give location permission'}/>
         )}
       </>
     );
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName} = seasonConfig[season];
+ 
+  return <div className={`${season} seasonDisplay`}>
+    <i className={`spinIcon iconLeft ${iconName} massive icon`}></i>
+    <h1 className="text">{text}</h1>
+    <i className={`spinIcon iconRight ${iconName} massive icon`}></i>
+  </div>;
+};
+
+export default SeasonDisplay;
